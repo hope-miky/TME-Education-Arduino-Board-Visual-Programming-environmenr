cdd7886eec537821a77a4c6de70886435be52403
Tesfamichael1074 <hope.miky1074@gmail.com>
2021-03-14 15:49:47 +0300

    added advanced playground html

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="blocklyDiv" style="height: 480px; width: 100%;"></div>
+
+  <xml id="toolbox" style="display: none">
+  <block type="object"/>
+<block type="member"></block>
+<block type="math_number"><field name="NUM">0</field></block>
+<block type="text"><field name="TEXT"/></block>
+<block type="logic_boolean"><field name="BOOL">TRUE</field></block>
+<block type="logic_null"/>
+<block type="lists_create_with"><mutation items="3"/></block>
+
+
+</xml>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Blockly from 'blockly';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+
+  mounted () {
+
+ Blockly.defineBlocksWithJsonArray([{
+  "type": "object",
+  "message0": "{ %1 %2 }",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "MEMBERS"
+    }
+  ],
+  "output": null,
+  "colour": 230,
+},
+{
+  "type": "member",
+  "message0": "%1 %2 %3",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "MEMBER_NAME",
+      "text": ""
+    },
+    {
+      "type": "field_label",
+      "name": "COLON",
+      "text": ":"
+    },
+    {
+      "type": "input_value",
+      "name": "MEMBER_VALUE"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+}]);
+
+// eslint-disable-next-line no-unused-vars
+const codelabGenerator = new Blockly.Generator('JSON');
+
+    Blockly.inject('blocklyDiv', {
+        toolbox: document.getElementById('toolbox'),
+        scrollbars: false
+      });
   }
 }
 </script>
